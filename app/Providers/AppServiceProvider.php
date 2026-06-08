cbfbb70b425807cbde2b9b9a3ff2a9a33eab20ed
Rheelz17 <rafliahmadf17@gmail.com>
2026-06-08 12:38:39 +0700

    fix http scheme railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (str_contains(request()->getHost(), 'ngrok')) {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
     }
